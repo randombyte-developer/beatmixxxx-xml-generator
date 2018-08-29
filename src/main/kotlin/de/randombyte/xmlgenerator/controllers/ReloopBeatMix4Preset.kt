db35c49ab787b2b56033b39559257a9535edf5cd
Sven Rahn <apps.randombyte@gmail.com>
2018-08-29 21:29:11 +0200

    Added more controls

--- src/main/kotlin/de/randombyte/xmlgenerator/controllers/ReloopBeatMix4Preset.kt
+++ src/main/kotlin/de/randombyte/xmlgenerator/controllers/ReloopBeatMix4Preset.kt
@@ -5,8 +5,10 @@ import de.randombyte.xmlgenerator.elements.ControllerPreset
 import de.randombyte.xmlgenerator.elements.Info
 import de.randombyte.xmlgenerator.elements.ScriptFile
 import de.randombyte.xmlgenerator.elements.control.Control
-import de.randombyte.xmlgenerator.elements.control.Groups
 import de.randombyte.xmlgenerator.elements.control.Options.SCRIPT_BINDING
+
+typealias Channels = List<Int>
+
 
 object ReloopBeatMix4Preset {
 
@@ -43,40 +45,66 @@ object ReloopBeatMix4Preset {
         )
     }
 
-    private val ALL_CHANNELS = (0x91..0x94).toList()
-
-    object ShiftOffsets {
-        val TRANSPORT = 0x40
-    }
+    private val LEFT_SIDE_CHANNELS = listOf(0x91, 0x93)
+    private val RIGHT_SIDE_CHANNELS = listOf(0x92, 0x94)
+    private val ALL_CHANNELS: Channels = LEFT_SIDE_CHANNELS + RIGHT_SIDE_CHANNELS
 
     private fun buildControls(): List<Control> {
-        val shift = control(scriptFunction("shift"), 0x20, SCRIPT_BINDING)
+        val buttons = ALL_CHANNELS.use {
+            val shift = control("shift", 0x20)
 
-        val transportControls = mapOf(
-                0x21 to "sync",
-                0x23 to "cue",
-                0x24 to "play"
-        ).flatMap { (midiNumber, key) ->
-            controlWithShift(scriptFunction(key), midiNumber, ShiftOffsets.TRANSPORT, SCRIPT_BINDING)
+            val transportControls = mapOf(
+                    0x21 to "sync",
+                    0x23 to "cue",
+                    0x24 to "play"
+            ).flatMap { (midiNumber, key) ->
+                controlWithShift(key, midiNumber, 0x40)
+            }
+
+            val loadTrack = controlWithShift("load", 0x50, -0x10)
+
+            return@use shift + transportControls + loadTrack
+        }
+
+        val fifthChannelControls = listOf(0xB5).use {
+            controlWithShift("traxRotate", 0x60, 0x10) + controlWithShift("crossfader", 0x2F, 0x30)
+        }
+
+        val fifthChannelButtons = listOf(0x95).use {
+            controlWithShift("traxPress", 0x09, 0x40)
+        }
+
+        val deckSwitchButtons = LEFT_SIDE_CHANNELS.use {
+            control("leftDeckSwitch", 0x28)
+        } + RIGHT_SIDE_CHANNELS.use {
+            control("rightDeckSwitch", 0x28)
         }
 
 
-        return shift + transportControls
+        return buttons + fifthChannelControls + fifthChannelButtons + deckSwitchButtons
     }
 
-    private fun control(key: String, midiNumber: Int, vararg options: String): List<Control> =
-            ALL_CHANNELS.mapIndexed { channel, status ->
+    // Utils
+
+    private fun <R> Channels.use(func: ChannelUtil.() -> R) = ChannelUtil(this).func()
+    private class ChannelUtil(val channels: Channels) {
+        fun control(key: String, midiNumber: Int) = control(key, midiNumber, channels)
+        fun controlWithShift(key: String, midiNumber: Int, shiftOffset: Int) = controlWithShift(key, midiNumber, shiftOffset, channels)
+
+    }
+
+    private fun control(key: String, midiNumber: Int, channels: Channels): List<Control> =
+            channels.mapIndexed { channel0based, status ->
                 Control(
-                        group = Groups.channel(channel + 1),
-                        key = key,
+                        key = scriptFunction(key),
                         status = status,
                         midiNumber = midiNumber,
-                        options = options.toSet()
+                        options = setOf(SCRIPT_BINDING)
                 )
             }
 
-    private fun controlWithShift(key: String, midiNumber: Int, shiftOffset: Int, vararg options: String): List<Control> {
-        val controls = control(key, midiNumber, *options)
+    private fun controlWithShift(key: String, midiNumber: Int, shiftOffset: Int, channels: List<Int>): List<Control> {
+        val controls = control(key, midiNumber, channels)
 
         val shiftedControls = controls.map { control ->
             control.copy(midiNumber = control.midiNumber + shiftOffset)
@@ -85,8 +113,8 @@ object ReloopBeatMix4Preset {
         return controls + shiftedControls
     }
 
-    private fun scriptFunction(functionName: String): String {
-        val finalFunctionName = "control" + functionName.first().toUpperCase() + functionName.substring(startIndex = 1)
-        return "$MIDI_INPUT_PREFIX.$finalFunctionName"
+    private fun scriptFunction(name: String): String {
+        val functionName = "control" + name.first().toUpperCase() + name.substring(startIndex = 1)
+        return "$MIDI_INPUT_PREFIX.$functionName"
     }
 }
